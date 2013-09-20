5ca28f0f20c7b4ab5a1198546642e8bf7be56112
takeshi0 <takehsi@ogawa.mobi>
2013-09-20 18:26:25 +0900

    \\

--- Common/ParseChText4Mp.cpp
+++ Common/ParseChText4Mp.cpp
@@ -209,13 +209,13 @@ BOOL CParseChText4::SaveChText(LPCWSTR filePath)
 		return FALSE;
 	}
 
-	std::wregex re(L".+\\(.+)\(.+\)\.ChSet4\.txt$");
+	std::wregex re(L"\\\(.+)\(.+\)\.ChSet4\.txt$");
 	this->tunerName = NULL;
-
-	std::wstring text(str);
+/*
+	std::wstring text(loadFilePath);
 	std::wsmatch m;
 	if( std::regex_search(text, m, re) ) this->tunerName = m[1];
-
+*/
 
 
 
